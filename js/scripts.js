f2dac9c9856c68ada1a224ae362d8122aadbd93a
hani1980 <hmunshi@engr-134-06.eastern.ewu.edu>
2014-10-20 13:37:46 -0700

    version 1

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+// Foundation 5 Activate! 
+$(document).foundation();
+
+
+// On page load, do this
+$(document).ready(function(){
+  
+  $('#one').corner('curl');
+  $('#two').corner('jut');
+   $('#three').corner('dogfold');
+
+});
